8af973366a6ee8106f63fa0831a9aa5357022ebd
Oswalt Mosby <0218710@up.edu.mx>
2021-01-05 12:39:16 -0600

    text login and sign up changed to english

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
         
         <!-- <img class="" style="width:15%;" src="img/logo.png" alt="logo"> -->
         <h1 class="py-5 me-auto">No Mercy Hospital</h1>
-        <a class="btn my-5 " style="height:1%;" href="">Iniciar sesion</a>
-        <a class="btn my-5 " style="height:1%;" href="">Registrarse</a>
+        <a class="btn my-5 " style="height:1%;" href="">Log in</a>
+        <a class="btn my-5 " style="height:1%;" href="">Sign up</a>
         
     </div>
     
