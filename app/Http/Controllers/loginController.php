e7ddcc6c525ea6d559504e5e7792f9600384597a
Oswalt Mosby <0218710@up.edu.mx>
2021-01-06 11:28:28 -0600

    Login model and controller added

---
+++ app/Http/Controllers/loginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Login;
+
+class loginController extends Controller
+{
+    public function login(){
+
+    }
+
+
+    public function logout(){
+
+    }
+
+    public function isLogged(){
+        
+    }
+}
